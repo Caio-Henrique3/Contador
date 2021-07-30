3611748c1a638904b0a34c0b04d9c080496a31e1
Caio Henrique <71612389+Caio-Henrique3@users.noreply.github.com>
2021-07-30 18:06:17 -0300

    Commit do projeto finalizado.

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Alarme ou Timer
+
+## Sobre
+Este projeto tem como finalidade desenvolvimento e aprendizado da linguagem Java Script.
+Nele temos um simples contador, que vai de um número inicial até final em uma sequência 
+pré determinada pelo usuário. 
+Aproveitando da realização deste pequeno projeto, esta sendo estudados alguns 
+conceitos de HTML e também CSS
+E por último estou aprendendo alguns conceitos de uso do prórpio Git Hub!!
+
+## Layout
+<img width="450" heigth="500" src="src/images/image1.png"> 
+<img width="450" heigth="500" src="src/images/image2.png"> 
+<img width="450" heigth="500" src="src/images/image3.png"> 
+
+## Tecnologias utilizadas
+Foram usadas apenas estruturação e desenvolvimento de linguagens e ferramenetas para Front-End
+neste projeto. Elas como já citadas anteriormente são:
+
+ - Java Script;
+ - HTML;
+ - CSS.
+
+## Como contribuir
+Se você é um desenvolvedor com mais experiência, sempre tem algumas dicas para dar aos mais novos,
+então no que puder acrescentar ao meu trabalho e ao meu crescimento como um desenvolvedor, estarei 
+sempre de portas abertas para ouvir aos conselhos e ensinamentos que tiveram para dar!!
+
+## Autor 
+
+ - Caio Henrique Marques da Silva
+ - LinkedIn: https://www.linkedin.com/in/caio-henrique-56b713200/
+ - Instagram: caiohenrique3
---
+++ src/script.js
@@ -0,0 +1,49 @@
+function contador(){
+    var inicio = document.getElementById('inicio');
+    var fim = document.getElementById('fim');
+    var passo = document.getElementById('passo');
+    var resultado = document.getElementById('resultado');
+    var progress = document.getElementById('progresso');
+    resultado.style.display = 'block';
+    resultado.style.textAlign = 'center';
+    resultado.style.marginTop = '10px';
+
+    if (inicio.value.length == 0){
+        resultado.innerHTML = `Não é possível iniciar o contador sem o número de início`;
+    } else if (fim.value.length == 0) {
+        resultado.innerHTML = `Não é possível iniciar o contador sem o número final`;
+    } else if (passo.value.length == 0) {
+        resultado.innerHTML = `Não é possível iniciar o contado sem o número de passo`;
+    } else {
+        resultado.innerHTML = ` `;
+        progress.style.display = 'block';
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+
+        intervalo = setTimeout(function(){
+            if (i < f) {
+                progress.style.display = 'none';
+                for (var cont = i; cont <= f; cont += p) {
+                    resultado.innerHTML += ` ${cont} 👉🏽`;
+                }
+            } else {
+                progress.style.display = 'none';
+                for (var cont = i; cont >= f; cont -= p){
+                    resultado.innerHTML += ` ${cont} 👉🏽`;
+                }
+            }    
+            resultado.innerHTML += ` 🏁`;
+        }, 5000);
+    }
+}
+
+function iniciar() {
+    var progress = document.getElementById('progresso');
+    var resultado = document.getElementById('resultado');
+    
+    progress.style.display = 'none';
+    resultado.style.display = 'none';
+}
+
+iniciar();
